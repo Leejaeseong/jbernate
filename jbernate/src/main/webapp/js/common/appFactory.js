00fc6de7bdf58d1f33fc75256e2512ac66837af9
AmadeusMK <amadeusmk@gmail.com>
2015-06-08 01:43:28 +0900

    .

--- jbernate/src/main/webapp/js/common/appFactory.js
+++ jbernate/src/main/webapp/js/common/appFactory.js
@@ -1,12 +1,17 @@
 /** 로딩 인터셉터 선언 */
 app.factory('globalLodingInterceptor', function ($q, $window) {
 	return function (promise) {
-		return promise.then(function (response) {
-			$("#globalLoadingImgId").hide();
-			return response;
-		}, function (response) {
-			$("#globalLoadingImgId").hide();
-			return $q.reject(response);
-		});
+		return promise.then(		
+			  // 성공
+			  function (response) {
+					$("#globalLoadingImgId").hide();
+					return response;
+			  	}
+  			  // 실패
+			, function (response) {
+					$("#globalLoadingImgId").hide();
+					return $q.reject(response);
+				}
+		);
 	};
 });
